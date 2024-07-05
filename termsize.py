1a1455b82522a327ea5a436164e9d4ac74bc2431
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-07-05 17:43:17 +0200

    Make code more readable

--- termsize.py
+++ termsize.py
@@ -2,16 +2,29 @@
 
 # Procedure for gathering terminal screen size
 #
-# Magnetic-Fox, 30.06.2024
+# Magnetic-Fox, 30.06.2024, 05.07.2024
+#
+# (C)2024 Bartłomiej "Magnetic-Fox" Węgrzyn
 
+import ansi
 import readchar
 
+# Procedure for gathering terminal's screen size
 def getTermSize():
 	input=""
-	print("\x1b7\x1b[999;999H\x1b[6n\x1b8\x1bM")
+
+	# Do the algorithm - set cursor position to 999x999 and then ask terminal for its position (which will automatically be changed to the possible maximum)
+	ansi.saveCurPos()
+	ansi.setCurPos(999,999)
+	ansi.askCurPos()	# Terminal will answer automatically, regardless of what comes next
+	ansi.restoreCurPos()
+	ansi.reverseLineFeed()	# I put reverse line feed here, because, for some strange reason,
+	print("")		# the whole code works only when the next line is passed
+
 	while True:
 		input2=readchar.readkey()
 		input+=input2
+		# You'll be able to interrupt this code if terminal haven't responded - just press ENTER
 		if(input2[-1]=='R') or (input2[-1]==readchar.key.ENTER):
 			break
 
