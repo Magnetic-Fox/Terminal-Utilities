08e6b5602da40615755462a0a43adc6b74baf8d0
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-11-16 16:32:00 +0100

    Fix bug with startIndex not properly setting display position; huge code look and working improvements

--- listutil.py
+++ listutil.py
@@ -56,6 +56,58 @@ def selectAndExecute(pos_x, pos_y, selection, displayPos, lcm, rcm, leftCharMod,
 		ansi.setCurPos(pos_x+(maxStrWidth if not placeCursorAtStart else 0)+cursorCorrection,pos_y+selection-displayPos)
 	return
 
+# Helper function to calculate proper display position
+def calculateDisplayPosition(selection, displayPos, s_height, listSize, up=True):
+	# Calculate while going up
+	if up:
+		while selection<displayPos:
+			displayPos-=s_height
+		if displayPos<0:
+			displayPos=0
+	# Calculate while going down
+	else:
+		while selection>=displayPos+s_height:
+			displayPos+=s_height
+		if(displayPos+s_height-1>=listSize):
+			displayPos=listSize-s_height
+	# Return calculated display position
+	return displayPos
+
+# Helper function to reset/calculate proper selection index
+def resetSelectionIndex(selection, list, listSize, up=True):
+	# Reset while going up
+	if up:
+		while list[selection]==None:
+			selection-=1
+			# Go down if beginning reached
+			if selection<0:
+				selection=0
+				while list[selection]==None:
+					selection+=1
+					# Stop if end reached
+					if selection>=listSize:
+						selection=listSize-1
+						break
+				# And then break
+				break
+	# Reset while going down
+	else:
+		while list[selection]==None:
+			selection+=1
+			# Go up if end reached
+			if selection>=listSize:
+				selection=listSize-1
+				while list[selection]==None:
+					selection-=1
+					# Stop if beginning reached
+					if selection<0:
+						selection=0
+						break
+				# And then break
+				break
+	# Return reset selection index
+	return selection
+
 # LIST SELECTOR UTILITY
 #
 # ARGUMENTS ARE AS FOLLOWS: list variable (list), x position of list on screen (pos_x),
@@ -68,7 +120,10 @@ def selectAndExecute(pos_x, pos_y, selection, displayPos, lcm, rcm, leftCharMod,
 # ONLY FIRST FIVE PARAMETERS ARE REQUIRED (list, pos_x, pos_y, s_width and s_height), OTHERS ARE OPTIONAL.
 #
 # IMPORTANT: Variables for position on screen starts from 1 (not 0!)
-def choice(list, pos_x, pos_y, s_width, s_height, addMargins=True, marginSize=1, leftCharMod='', rightCharMod='', onSelection=None, leftSideCoordinates=True, placeCursorAtStart=False, cursorCorrection=0, startIndex=0, returnCoordinatesToo=False):
+def choice(list, pos_x, pos_y, s_width, s_height, addMargins=True, marginSize=1, leftCharMod='', rightCharMod='', onSelection=None, leftSideCoordinates=True, placeCursorAtStart=False, cursorCorrection=0, startIndex=0, returnCoordinatesToo=False, firstRedrawStartIndexOnly=False):
+	# Let's thrown an exception yet at the beginning (if needed)
+	if list==None:
+		raise TypeError("list is None")
 	# Define temporary variables and initialize them
 	s_width-=pos_x-1
 	s_height-=pos_y-1
@@ -79,11 +134,8 @@ def choice(list, pos_x, pos_y, s_width, s_height, addMargins=True, marginSize=1,
 	redrawAll=True
 	reset=False
 	allNone=True
-
-	# Test if list is not None
-	if list!=None:
-		list=list[:]
-
+	once=firstRedrawStartIndexOnly
+	list=list[:]
 	# Test if leftCharMod is one character long
 	if leftCharMod!=None and len(leftCharMod)>1:
 		# Throw exception
@@ -91,31 +143,24 @@ def choice(list, pos_x, pos_y, s_width, s_height, addMargins=True, marginSize=1,
 	# Test if rightCharMod is one character long
 	elif rightCharMod!=None and len(rightCharMod)>1:
 		# Throw exception
-		raise ValueError("rightCharMod had to be one-character string")
+		raise ValueError("rightCharMod has to be one-character string")
 	else:
 		# Let's shorten it
 		lcm=(leftCharMod!=None) and (len(leftCharMod)==1)
 		rcm=(rightCharMod!=None) and (len(rightCharMod)==1)
-
+	# Store list size in variable
+	listSize=len(list)
 	# Set the real start index on existing list if selection points to the None
-	if (list!=None) and (selection<len(list)) and (selection>=0):
-		while list[selection]==None:
-			selection+=1
-			if selection>=len(list):
-				selection=startIndex
-				while list[selection]==None:
-					selection-=1
-					if selection<0:
-						selection=0
-						break
-				break
+	if selection<len(list) and selection>=0:
+		selection=resetSelectionIndex(selection,list,listSize,up=False)
 	else:
 		selection=0
-
+	# Check if screen position is good
+	if selection>=displayPos+s_height:
+		displayPos=calculateDisplayPosition(selection,displayPos,s_height,listSize,up=False)
 	# Run the list selector utility only on existing list that has at least one element
-	if(list!=None) and (len(list)>0):
+	if len(list)>0:
 		# Test and prepare list elements
-		listSize=len(list)
 		for x in range(len(list)):
 			# Check if list is None-only and if it is necessary to reset the selector utility
 			if list[x]==None:
@@ -131,15 +176,12 @@ def choice(list, pos_x, pos_y, s_width, s_height, addMargins=True, marginSize=1,
 						if displayPos+s_height>len(list):
 							displayPos=len(list)-s_height
 					reset=False
-
 			# Add margins to the list elements
 			if addMargins:
 				list[x]=" "*marginSize+list[x]+" "*marginSize
-
 			# Update maximum string width (length)
 			if len(list[x])>maxStrWidth:
 				maxStrWidth=len(list[x])
-
 			# Truncate too long elements
 			if maxStrWidth>s_width:
 				maxStrWidth=s_width
@@ -148,23 +190,26 @@ def choice(list, pos_x, pos_y, s_width, s_height, addMargins=True, marginSize=1,
 					subt+=marginSize
 				list[x]=list[x][0:s_width-subt]
 				list[x]+="..."+" "*marginSize
-
 		# Throw exception (None-only list)
 		if allNone:
 			raise ValueError("list contains None elements only")
-
 		# Set how much to add to the output coordinates in "on selection" event
 		if not leftSideCoordinates:
 			adds=maxStrWidth-1
-
 		# Main list selector part
 		while not allNone:
 			# Full list redraw event
 			if redrawAll:
-				# Set initial position
-				ansi.setCurPos(pos_x,pos_y)
+				if not once:
+					# Set initial position
+					ansi.setCurPos(pos_x,pos_y)
 				# Draw elements up to the maximum height
 				for x in range(s_height):
+					if once:
+						if displayPos+x<startIndex:
+							continue
+						else:
+							ansi.setCurPos(pos_x,pos_y+x)
 					if x==listSize:
 						break
 					else:
@@ -190,134 +235,94 @@ def choice(list, pos_x, pos_y, s_width, s_height, addMargins=True, marginSize=1,
 							if(onSelection!=None):
 								onSelection(selection,pos_x+adds,pos_y+x)
 								ansi.setCurPos(pos_x,pos_y+x+1)
+							if once:
+								once=False
+								break
 				# Set redraw all task as done
 				redrawAll=False
 				# Set cursor position after drawing the list
 				ansi.setCurPos(pos_x+(maxStrWidth if not placeCursorAtStart else 0)+cursorCorrection,pos_y+selection-displayPos)
-
 			# Flush standard output (which is VERY IMPORTANT!)
 			sys.stdout.flush()
 			# Read character from the standard input
 			inp=readchar.readkey()
-
 			# Go up on the list (one element, 16 elements, get first element of the list)
 			if(inp==readchar.key.UP) or (inp==readchar.key.PAGE_UP) or (inp==readchar.key.HOME):
 				# Store old selection index
 				oldSelection=selection
-
 				# Go up one element
 				if inp==readchar.key.UP:
 					selection-=1
-
 				# Go up 16 elements
 				elif inp==readchar.key.PAGE_UP:
 					selection-=16
-
 				# Go up to the first element
 				elif inp==readchar.key.HOME:
 					selection=0
-
 				# Reset selection to 0 if selection is lower
 				if selection<0:
 					selection=0
 					# If list selection hasn't changed - do nothing
 					if oldSelection==selection:
 						continue
-
 				# Reset position while on None elements
-				while list[selection]==None:
-					selection-=1
-					if selection<0:
-						selection=0
-						while list[selection]==None:
-							selection+=1
-							if selection>=listSize:
-								selection=listSize-1
-								break
-						break
+				selection=resetSelectionIndex(selection,list,listSize)
+				# Continue on None
 				if list[selection]==None:
 					continue
-
 				# Additional test if selection actually not changed
 				if oldSelection==selection:
 					continue
-
 				# Calculate coordinates of the previous screen and let it be fully redrawn
 				if selection<displayPos:
-					while selection<displayPos:
-						displayPos-=s_height
-					if displayPos<0:
-						displayPos=0
+					displayPos=calculateDisplayPosition(selection,displayPos,s_height,listSize)
 					redrawAll=True
-
 				# Unselect previously selected element and select currently selected
 				else:
 					if(oldSelection<listSize) and (s_height>1):
 						unSelect(pos_x,pos_y,oldSelection,displayPos,lcm,rcm,leftCharMod,rightCharMod,maxStrWidth,list)
 					if(selection-displayPos>=0):
 						selectAndExecute(pos_x,pos_y,selection,displayPos,lcm,rcm,leftCharMod,rightCharMod,maxStrWidth,list,onSelection,adds,placeCursorAtStart,cursorCorrection)
-
 			# Go down on the list (one element, 16 elements, get end of the list)
 			elif(inp==readchar.key.DOWN) or (inp==readchar.key.PAGE_DOWN) or (inp==readchar.key.END):
 				# Store old selection index
 				oldSelection=selection
-
 				# Go down one element
 				if inp==readchar.key.DOWN:
 					selection+=1
-
 				# Go down 16 elements
 				elif inp==readchar.key.PAGE_DOWN:
 					selection+=16
-
 				# Go down to the last element
 				elif inp==readchar.key.END:
 					selection=listSize-1
-
 				# Reset selection to the last element if greater or equal
 				if selection>=listSize:
 					selection=listSize-1
 					# If list selection hasn't changed - do nothing
 					if oldSelection==selection:
 						continue
-
 				# Reset position while on None elements
-				while list[selection]==None:
-					selection+=1
-					if selection>=listSize:
-						selection=listSize-1
-						while list[selection]==None:
-							selection-=1
-							if selection<0:
-								selection=0
-								break
-						break
+				selection=resetSelectionIndex(selection,list,listSize,up=False)
+				# Continue on None
 				if list[selection]==None:
 					continue
-
 				# Additional test if selection actually not changed
 				if oldSelection==selection:
 					continue
-
 				# Calculate coordinates of the previous screen and let it be fully redrawn
 				if selection>=displayPos+s_height:
-					while selection>=displayPos+s_height:
-						displayPos+=s_height
-					if(displayPos+s_height-1>=listSize):
-						displayPos=listSize-s_height
+					displayPos=calculateDisplayPosition(selection,displayPos,s_height,listSize,up=False)
 					redrawAll=True
-
 				# Unselect previously selected element and select currently selected
 				else:
 					if(oldSelection>=0) and (pos_y+(oldSelection-displayPos)>=pos_y):
 						unSelect(pos_x,pos_y,oldSelection,displayPos,lcm,rcm,leftCharMod,rightCharMod,maxStrWidth,list)
 					if(selection-displayPos<s_height):
 						selectAndExecute(pos_x,pos_y,selection,displayPos,lcm,rcm,leftCharMod,rightCharMod,maxStrWidth,list,onSelection,adds,placeCursorAtStart,cursorCorrection)
-
 			# Break list selector (selection index is already set)
 			elif(inp==readchar.key.ENTER):
 				break
-
 	# An error occurred - throw appropriate exception
 	else:
 		if list==None:
@@ -326,7 +331,6 @@ def choice(list, pos_x, pos_y, s_width, s_height, addMargins=True, marginSize=1,
 			raise ValueError("list is empty")
 		else:
 			raise RuntimeError("unknown error occurred")
-
 	# Return selection or throw exception
 	if returnCoordinatesToo:
 		if selection<0:
@@ -353,42 +357,33 @@ def test(in1,in2,in3):
 def makeTest(width=80, height=24):
 	# Prepare test
 	list=["1",None,None,"2"]
-
 	# Insert some empty values at the beginning of the list
 	for x in range(1,100):
 		list+=[None]
-
 	# Insert some strings and empty values
 	for x in range(1,2001):
 		list+=[str(x)]
 		if x%6==0:
 			list+=[None]
-
 	# Insert some empty values at the end of the list
 	for x in range(1,100):
 		list+=[None]
-
 	# Prepare output terminal
 	ansi.clear()
 	ansi.setNoWrapAround()
 	ansi.setCurPos(30,1)
-
 	# Make some header
 	print("ID,X,Y")
-
 	# Invoke list selector and store selection in "chosen" variable
 	chosen,rx,ry=choice(list,1,1,width,height,onSelection=test,returnCoordinatesToo=True,leftCharMod="[",rightCharMod="]",marginSize=2)
-
 	# Display selection ID
 	ansi.clear()
 	ansi.setCurPos(1,1)
 	print("ID:   "+str(chosen))
 	print("X:    "+str(rx))
 	print("Y:    "+str(ry))
-
 	# Set wrap around, as probably was before running test
 	ansi.setWrapAround()
-
 	# Finish
 	return
 
